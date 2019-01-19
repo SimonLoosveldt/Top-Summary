531780d2d688903b17724d87ff8613160316fc8a
Simon Loosveldt <simon.loosveldt@student.odisee.be>
2019-01-19 20:00:58 +0100

    Making of class 'Leider'.

---
+++ Overview/Overview/Leider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overview
+{
+    class Leider
+    {
+
+        private String naam;
+        private String afdeling;
+
+
+
+        public enum Leidsters { HanneVl, Marie, Clémence, Laura, Leni, Hélène, Silke, Leonie, Leontine, HanneVb, Margot, Alexandra, Lotte };
+
+        public Leider (String naam, String afdeling)
+        {
+            SetNaam(naam);
+
+
+        }
+
+        public void SetNaam(String naam) { this.naam = naam; }
+        public String GetNaam() { return this.naam; }
+
+        public void Setafdeling(String naam) { this.naam = naam; }
+        public String Getafdeling() { return this.naam; }
+
+
+
+    }
+}
--- Overview/Overview/Program.cs
+++ Overview/Overview/Program.cs
@@ -10,6 +10,11 @@ namespace Tops_Program
     {
         static void Main(string[] args)
         {
+
+             
+
+
+
         }
     }
 }
