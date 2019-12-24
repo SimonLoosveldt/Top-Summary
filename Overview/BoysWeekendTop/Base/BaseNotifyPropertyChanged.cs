48c25717080007fb9ed2fa9d8a51af31e4262447
Michel Michels <michel.michels.jr@gmail.com>
2019-12-24 10:57:50 +0100

    WPF port in progress

---
+++ Overview/BoysWeekendTop/Base/BaseNotifyPropertyChanged.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BoysWeekendTop.Base
+{
+    public class BaseNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual void SetProperty<T>(ref T innerValue, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(innerValue, value))
+                return;
+
+            innerValue = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ Overview/BoysWeekendTop/ViewModels/AddTopViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using BoysWeekendTop.Base;
+using MaterialDesignThemes.Wpf;
+
+namespace BoysWeekendTop.ViewModels
+{
+    public class AddTopViewModel
+    {
+        public ICommand CloseCommand => new RelayCommand(() => Close());
+        
+        private void Close()
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
+        }
+    }
+}
