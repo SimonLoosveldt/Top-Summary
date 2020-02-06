abfd77db1e718c69dc2f5110df0ddac7f3a4dd7a
Simon Loosveldt <simon.loosveldt@student.odisee.be>
2020-02-06 15:15:12 +0100

    Update

--- Overview/BoysWeekendTop/Views/MainWindow.xaml.cs
+++ Overview/BoysWeekendTop/Views/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace BoysWeekendTop.Views
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
